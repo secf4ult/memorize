17aa9954b2653779c6cc061c6220553f4e66dcea
secf4ult <secf4ult@outlook.com>
2024-08-06 09:36:02 +0900

    Initial Commit

---
+++ MemorizeTests/MemorizeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeTests.swift
+//  MemorizeTests
+//
+//  Created by Yu Xiao on 8/6/24.
+//
+
+import Testing
+@testable import Memorize
+
+struct MemorizeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
